dd4110b39096b2478e4ef736c34f61e42f9aba76
zhezhaoxu <zhezhaoxu@tencent.com>
2019-07-21 01:58:44 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-# bazel build -c opt tensorflow_serving/... --jobs 4 --incompatible_disallow_data_transition=false --local_ram_resources=2048
-bazel build -c opt tensorflow_serving/... --jobs 4 --local_ram_resources=2048
+bazel build -c opt tensorflow_serving/... --jobs 4 --incompatible_disallow_data_transition=false
+#bazel build -c opt tensorflow_serving/... --jobs 4 --local_ram_resources=2048
